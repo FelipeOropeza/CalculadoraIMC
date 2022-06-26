10832eecacd104cd9f6208aeb7eafcc1d9cd0a23
FelipeOropeza <felipe.oropeza@etec.sp.gov.br>
2022-06-26 14:39:47 -0300

    Termino
    Data 26/06

--- app/src/main/java/com/example/atividadeimc/MainActivity.java
+++ app/src/main/java/com/example/atividadeimc/MainActivity.java
@@ -22,6 +22,45 @@ public class MainActivity extends AppCompatActivity {
     double n1, n2;
 
     public void Calcula (View view){
-        n1 = Double.parseDouble()
+        n1 = Double.parseDouble(num1.getText().toString());
+        n2 = Double.parseDouble(num2.getText().toString());
+        double resul = n1/(n2*n2);
+
+        if(resul > 43){
+            result.setText(resul + "\nObedidade Morbida");
+        }
+        else if(resul > 30 && resul < 39.9){
+            result.setText(resul + "\nObesidade Moderada");
+        }
+        else if(resul > 25 && resul < 29.9){
+            result.setText(resul + "\nObesidade Leve");
+        }
+        else if(resul > 20 && resul < 24.9){
+            result.setText(resul + "\nNormal");
+        }
+        else{
+            result.setText(resul + "\nAbaixo do Normal");
+        }
+    }
+    public void CalculaFe (View view){
+        n1 = Double.parseDouble(num1.getText().toString());
+        n2 = Double.parseDouble(num2.getText().toString());
+        double resul = n1/(n2*n2);
+
+        if(resul > 39){
+            result.setText(resul + "\nObedidade Morbida");
+        }
+        else if(resul > 29 && resul < 38.9){
+            result.setText(resul + "\nObesidade Moderada");
+        }
+        else if(resul > 24 && resul < 28.9){
+            result.setText(resul + "\nObesidade Leve");
+        }
+        else if(resul > 19 && resul < 23.9){
+            result.setText(resul + "\nNormal");
+        }
+        else{
+            result.setText(resul + "\nAbaixo do Normal");
+        }
     }
 }
